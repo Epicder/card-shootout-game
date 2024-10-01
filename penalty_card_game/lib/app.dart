504b52e2cca4bdd319e6fcd1aa3a5b8356b16015
Epicder <epicder11@gmail.com>
2024-09-30 21:56:35 -0300

    Added bloc, and starting with App screens ✔

---
+++ penalty_card_game/lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
+
+class MyApp extends StatelessWidget {
+  final UserRepository userRepository;
+  const MyApp(this.userRepository, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- penalty_card_game/lib/main.dart
+++ penalty_card_game/lib/main.dart
@@ -1,64 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:user_repository/user_repository.dart';
+import 'package:penalty_card_game/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  MyApp({super.key});
-  Future<FirebaseApp> get newMethod => Firebase.initializeApp();
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          // Check si la conexion con firebase es correcta
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const MyHomePage();
-          }
-
-          // Si hay un error
-          if (snapshot.hasError) {
-            return const Scaffold(
-              body: Center(
-                child: Text("Error inicializando Firebase"),
-              ),
-            );
-          }
-
-          // Mientras se espera la conexion
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Firebase Demo"),
-      ),
-      body: const Center(
-        child: Text("Firebase Initialized Successfully!"),
-      ),
-    );
-  }
+  await Firebase.initializeApp();
+  runApp(MyApp(FirebaseUserRepo()));
 }
