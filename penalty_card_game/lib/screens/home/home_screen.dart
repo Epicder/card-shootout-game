f43498788c6a8c9f30129b347fcf3c05508918e2
Epicder <epicder11@gmail.com>
2024-10-03 00:34:06 -0300

    Added adjustements to the Home Screen, Button for Play the mvp ✔

--- penalty_card_game/lib/screens/home/home_screen.dart
+++ penalty_card_game/lib/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:penalty_card_game/blocs/sign_in_bloc/sign_in_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,17 +11,30 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
 			appBar: AppBar(
 				title: const Text(
-					'Welcome, you are In !'
-				),
+					'MAIN MENU Penalty Card Shootout!'
+      ),
+        
 				actions: [
 					IconButton(
 						onPressed: () {
-							//context.read<SignInBloc>().add(const SignOutRequired());
+							context.read<SignInBloc>().add(const SignOutRequired());
 						}, 
 						icon: Icon(Icons.login)
-					)
+					),
 				],
+        
 			),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()), // En vez de HomeScreen el mvp
+            );
+          },
+          child: const Text('Play'),
+        ),
+        ),
 		);
   }
 }
